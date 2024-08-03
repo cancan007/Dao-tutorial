d768def1d187727e8b36f2615a261cee9303630d
cancan007 <shota.moue0211@gmail.com>
2024-08-03 11:17:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+参考: https://docs.openzeppelin.com/contracts/5.x/governance
+参考 ②: https://qiita.com/lowzzy/items/5753c424399be5c41c30
+参考 ③: https://docs.openzeppelin.com/contracts/5.x/api/governance
+全体像参考: https://moralis.io/how-to-create-a-dao-in-10-minutes/
---
+++ contracts/TimeLock.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+    constructor(
+        uint minDelay,
+        address[] memory proposers,
+        address[] memory executors,
+        address admin // optional: deactivate with 0 adress
+    )TimelockController(minDelay,proposers,executors, admin) {
+    }
+}
